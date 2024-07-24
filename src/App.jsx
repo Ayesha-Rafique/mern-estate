8fcd1c48b24d74bc5c94795c554678bee2c39fa2
Ayesha-Rafique <as3024116@gmail.com>
2024-07-24 15:15:27 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Signin from "c:/Users/Night Agent/Desktop/Ayesha/mern-estate/client/src/pages/signin"
+import About from "c:/Users/Night Agent/Desktop/Ayesha/mern-estate/client/src/pages/about"
+import Profile from "c:/Users/Night Agent/Desktop/Ayesha/mern-estate/client/src/pages/profile"
+import Signup from "c:/Users/Night Agent/Desktop/Ayesha/mern-estate/client/src/pages/SignUp"
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
