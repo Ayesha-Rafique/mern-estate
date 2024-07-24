b2d0b0ef90e986453baeb1bd5dbc5caac37341b0
Ayesha-Rafique <as3024116@gmail.com>
2024-07-24 17:46:30 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Signin from "c:/Users/Night Agent/Desktop/Ayesha/mern-estate/client/src/p
 import About from "c:/Users/Night Agent/Desktop/Ayesha/mern-estate/client/src/pages/about"
 import Profile from "c:/Users/Night Agent/Desktop/Ayesha/mern-estate/client/src/pages/profile"
 import Signup from "c:/Users/Night Agent/Desktop/Ayesha/mern-estate/client/src/pages/SignUp"
+import Header from "./component/Header"
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<Signin />} />
